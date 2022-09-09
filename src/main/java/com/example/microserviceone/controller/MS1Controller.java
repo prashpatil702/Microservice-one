5774b873a6239ba0a6dce1ca434fb10f7943f615
prashpatil702 <prashpatil702@gmail.com>
2022-09-09 19:24:07 +0530

    Added headers

--- src/main/java/com/example/microserviceone/controller/MS1Controller.java
+++ src/main/java/com/example/microserviceone/controller/MS1Controller.java
@@ -1,7 +1,10 @@
 package com.example.microserviceone.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microserviceone.config.Config;
@@ -11,10 +14,22 @@ public class MS1Controller {
 	
 	@Autowired
 	private Config config;
+	
+	@Autowired
+	private HttpServletRequest request;
 
 	@GetMapping("/endPoint")
 	public String getValue()
 	{
 		return config.getValue();
 	}
+	
+	@GetMapping("/getMessageFromMS1")
+	public String getMessage()
+	{
+		String token = request.getHeader("X-Auth-Code");
+		//return "Start";
+		return "Start"+" "+token;
+	}
+	
 }
