53d4db46c467bfb3ec0fae660b6379086e4c9e2e
prashpatil702 <prashpatil702@gmail.com>
2022-09-09 22:35:44 +0530

    Added eureka client

--- src/main/java/com/example/microserviceone/MicroserviceOneApplication.java
+++ src/main/java/com/example/microserviceone/MicroserviceOneApplication.java
@@ -2,8 +2,10 @@ package com.example.microserviceone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceOneApplication {
 
 	public static void main(String[] args) {
